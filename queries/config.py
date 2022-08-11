baed415bbbc04f47989efb17af74f725067556bc
Joanna-Turner <50106455+Joanna-Turner@users.noreply.github.com>
2022-08-11 17:12:16 -0400

    Update config.py

--- queries/config.py
+++ queries/config.py
@@ -1,8 +1,8 @@
 NAUTICAL_TIME_RASTER_TABLE = 'global-fishing-watch.paper_global_longline_sets.nautical_time_raster_v20211117'
 
-LONGLINE_FEATURES_TABLE = 'world-fishing-827.machine_learning_production.afd_features_24hr_v20210923'
+LONGLINE_FEATURES_TABLE = 'machine_learning_production.afd_features_24hr_v20210923'
 
-LONGLINE_POSITION_SCORE_TABLE = 'world-fishing-827.machine_learning_dev_ttl_120d.longline_fishing_'
+LONGLINE_POSITION_SCORE_TABLE = 'global-fishing-watch:paper_global_longline_sets.longline_fishing_'
 
 LONGLINE_EVENTS_TABLE = 'global-fishing-watch.paper_global_longline_sets.longline_events_smoothv20220801_'
 
